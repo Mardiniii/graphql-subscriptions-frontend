7ccbc2bf6c494c6609587cadcaf6da0709c3e22d
Sebastian Zapata Mardini <sezama03@hotmail.com>
2020-08-21 12:57:18 -0400

    Links component subscribes to the "new_link" subscription

--- src/Links.js
+++ src/Links.js
@@ -13,13 +13,46 @@ const LINKS_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+    }
+  }
+`
+
 class Links extends Component {
+  _subscribeToNewLinks = subscribeToMore => {
+    if (!this.subscribed) {
+      this.subscribed = true
+      subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev
+          console.log(subscriptionData, prev)
+
+          const newLink = subscriptionData.data.newLink;
+          const exists = prev.links.find(({ id }) => id === newLink.id);
+          if (exists) return prev;
+
+          return Object.assign({}, prev, {
+            links: [newLink, ...prev.links],
+            __typename: prev.links.__typename
+          })
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <Query query={LINKS_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Loading...</div>
           if (error) return <div>Error</div>
+          this._subscribeToNewLinks(subscribeToMore)
 
           const linksToRender = data.links
 
